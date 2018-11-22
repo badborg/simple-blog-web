038d1d89dd4e28e7d6da6ceee8566e5193d1a4b6
bb <badborg@gmail.com>
2018-11-22 15:46:57 +0700

    logo

---
+++ server/src/server/logo.clj
@@ -0,0 +1,43 @@
+(ns server.logo
+  (:require [clojure.java.io :as io]
+            [environ.core :refer [env]]))
+
+(def logo-file
+  (env :logo))
+
+(defn logo-ext
+  []
+  (when logo-file
+    (some-> (re-find #"[a-z]+$" logo-file)
+            vector
+            set
+            (some #{"png" "svg"}))))
+
+(defn path
+  []
+  (let [ext (logo-ext)]
+    (cond-> "/logo"
+      ext (str "." ext))))
+
+(defn content-type
+  []
+  (-> {"svg" "image/svg+xml"
+       "png" "image/png"}
+      (get (logo-ext))
+      (or "application/octet-stream")))
+
+(defn logo-content
+  []
+  (when logo-file
+    (let [file (io/file logo-file)]
+      (when (.exists file)
+        file))))
+
+(def available?
+  logo-content)
+
+(defn response
+  []
+  (when-let [content (logo-content)]
+    {:headers {"Content-Type" (content-type)}
+     :body content}))
