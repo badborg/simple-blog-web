65a5177e3f0e83d48f9b591e60d3cc2ef4f34e5a
bb <badborg@gmail.com>
2018-11-22 15:36:12 +0700

    add dependencies

--- server/project.clj
+++ server/project.clj
@@ -1,19 +1,21 @@
 (defproject server "0.1.0-SNAPSHOT"
   :description "Web server for simple blog web"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 ;; [cheshire "5.8.0"]
-                 ;; [clj-time "0.14.2"]
+                 [cheshire "5.8.0"]
+                 [clj-time "0.14.2"]
                  [compojure "1.6.0"]
                  [environ "1.1.0"]
-                 ;; [hiccup "1.0.5"]
+                 [hiccup "1.0.5"]
                  [http-kit "2.2.0"]
                  [ring/ring-defaults "0.3.1"]
+                 [ring/ring-codec "1.1.1"]
                  [ring/ring-core "1.6.3"]
                  [ring/ring-json "0.4.0"]
-                 ;; [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "20180219.1"]
+                 [com.googlecode.owasp-java-html-sanitizer/owasp-java-html-sanitizer "20180219.1"]
                  ]
   :plugins [[lein-environ "1.1.0"]]
   :main server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :test {:resource-paths ["test-resources"]}})
+             :test {:dependencies [[ring/ring-mock "0.3.2"]]
+                    :resource-paths ["test-resources"]}})
