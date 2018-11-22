0d1e875d5533f0b79ca6bd8d4b9fa0c093700979
bb <badborg@gmail.com>
2018-11-22 19:35:44 +0700

    client: project

---
+++ client/project.clj
@@ -0,0 +1,16 @@
+(defproject client "0.1.0-SNAPSHOT"
+  :description "Web client for simple blog web"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [org.clojure/core.async "0.4.474"]
+                 [cljs-ajax "0.7.4"]
+                 ;; [com.cognitect/transit-clj "0.8.313"]
+                 [rum "0.11.2"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-dir "../server/resources/js"
+                                   :optimizations :simple
+                                   ;; :optiomizations :advanced
+                                   ;; :pretty-print false
+                                   :modules {:cljs-base
+                                             {:output-to "../server/resources/public/js/core.js"}}}}]})
