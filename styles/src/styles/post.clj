a765802cc86a60bee37daeba868c921aa0c74a72
bb <badborg@gmail.com>
2018-11-25 21:02:47 +0700

    styles cleanup post

--- styles/src/styles/post.clj
+++ styles/src/styles/post.clj
@@ -31,9 +31,7 @@
 
 (defstyles post
   [:.post
-   {
-    ;; :width "60%"
-    :float "left"}
+   {:float "left"}
    post-content/main
    tags
    [:.image
@@ -47,17 +45,7 @@
       [:.related [:.posts [:.post]]])))
 
 (def post-media-ranges
-  {
-   ;; [0 800] [:.post
-   ;;          {:width (% 100)}]
-   ;; [800 900] [:.post
-   ;;             {:width (% 60)}]
-   ;; [900 1300] [:.post
-   ;;             {:width (% 66.66)}]
-   ;; [1300 nil] [:.post
-   ;;             {:width (% 50)}]
-
-   [0 800] [:.post
+  {[0 800] [:.post
             {:width (% 100)}]
    [800 1200] [:.post
                {:width (% 66.66)}]
@@ -107,76 +95,4 @@
   (build-media content-media-ranges)
   (build-media post-media-ranges)
   (build-media font-media-ranges)
-
-  #_(at-media
-    {:screen :only
-     :max-width "1050px"}
-    [:.post
-     [:.content
-      {:max-width "500px"}]])
-  #_(at-media
-    {:screen :only
-     :max-width "900px"}
-    [:.post
-     [:.content
-      {:max-width "400px"}]])
-  #_(at-media
-    {:screen :only
-     :max-width "800px"}
-    [:.post
-     {:width "100%"}
-     [:.content
-      {:max-width "600px"}]]
-    #_[:.related
-     [:.posts
-      [:.post :#ad2
-       {:width "50%"}]]]
-    )
-  #_(at-media
-    {:screen :only
-     :max-width "650px"}
-    [:.post
-     [:.content
-      {:max-width "500px"}]])
-  #_(at-media
-    {:screen :only
-     :max-width "600px"}
-    [:.post
-     {:width "100%"}]
-    #_[:.related
-     [:.posts
-      [:.post :#ad2
-       {:width "100%"}]]])
-  #_(at-media
-    {:screen :only
-     :max-width "550px"}
-    [:.post
-     [:.content
-      {:max-width "400px"}]])
-  #_(at-media
-    {:screen :only
-     :max-width "450px"}
-    [:.post
-     [:.content
-      {:max-width "300px"
-       :font-size "0.9em"}]])
-  #_(at-media
-    {:screen :only
-     :max-width "400px"}
-    [:h1.post
-     {:font-size "1em"}]
-    [:.post
-     [:.content
-      {:font-size "0.8em"}]
-     [:.tags
-      [:.tag
-       {:font-size "0.8em"}]]])
- #_ (at-media
-    {:screen :only
-     :min-width "1300px"}
-    [:.post
-     {:width "50%"}]
-    #_[:.related
-     [:.posts
-      [:.post :#ad2
-       {:width "25%"}]]]))
+  )
