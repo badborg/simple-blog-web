cc0225222066e7721b3ded1888c75feab167a2cf
bb <badborg@gmail.com>
2018-11-25 20:54:27 +0700

    styles: cleanup headings

--- styles/src/styles/headings.clj
+++ styles/src/styles/headings.clj
@@ -14,25 +14,10 @@ g(ns styles.headings
   [:h1.search
    {:font-style "oblique"}]
   (build-media
-    {[0 800] [:h1
-              {:font-size (em 1.7)}]
-     [0 500] [:h1
-              {:font-size (em 1.5)}]
-     [0 400] [:h1
-              {:font-size (em 1.2)}]})
-  ;; (at-media
-  ;;   {:screen :only
-  ;;    :max-width "800px"}
-  ;;   [:h1
-  ;;    {:font-size "1.7em"}])
-  ;; (at-media
-  ;;   {:screen :only
-  ;;    :max-width "500px"}
-  ;;   [:h1
-  ;;    {:font-size "1.5em"}])
-  ;; (at-media
-  ;;   {:screen :only
-  ;;    :max-width "400px"}
-  ;;   [:h1
-  ;;    {:font-size "1.2em"}])
+    {[0 400] [:h1
+              {:font-size (em 1.2)}]
+     [400 500] [:h1
+                {:font-size (em 1.5)}]
+     [500 800] [:h1
+                {:font-size (em 1.7)}]})
   )
