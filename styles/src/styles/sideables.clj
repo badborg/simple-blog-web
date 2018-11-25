ecfb0c46249311b56f2d5a5e7f359f131fc43a07
bb <badborg@gmail.com>
2018-11-25 21:01:08 +0700

    styles cleanup sideables

--- styles/src/styles/sideables.clj
+++ styles/src/styles/sideables.clj
@@ -3,18 +3,7 @@
 
 (defn media-ranges
   [sels]
-  {
-   ;; [0 600] (add-sels sels
-   ;;                   {:width (% 100)})
-   ;; [600 800] (add-sels sels
-   ;;                   {:width (% 50)})
-   ;; [800 900] (add-sels sels
-   ;;                      {:width (% 40)})
-   ;; [900 1300] (add-sels sels
-   ;;                      {:width (% 33.33)})
-   ;; [1300 nil] (add-sels sels
-   ;;                      {:width (% 25)})
-   [0 600] (add-sels sels
+  {[0 600] (add-sels sels
                      {:width (% 100)})
    [600 800] (add-sels sels
                      {:width (% 50)})
